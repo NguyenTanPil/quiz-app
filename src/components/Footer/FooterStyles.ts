405cc114b46506b746f00f79845b2ade14d7aeec
NguyenTanPil <traila1008@gmail.com>
2022-08-23 23:39:11 +0700

    update footer component

--- src/components/Footer/FooterStyles.ts
+++ src/components/Footer/FooterStyles.ts
@@ -1,45 +1,131 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import carImg from '../../images/car.gif';
+import cycleImg from '../../images/cycle.gif';
+import footerBg from '../../images/footerBg.png';
+import { breakpoints, devices } from '../../styles/breakpoints';
 
 export const Container = styled.footer`
-  background-color: #f7f7f7;
-  padding-bottom: 4.8rem;
-  padding-top: 4.8rem;
+  background-color: #fbfbfd;
+  overflow-x: hidden;
+  padding: 4rem 0 28rem;
+  position: relative;
 `;
 
-export const Content = styled.div`
+export const FooterTop = styled.div`
   display: flex;
-  margin-left: auto;
-  margin-right: auto;
-  max-width: 112rem;
+  flex-wrap: wrap;
+  margin-left: -1.6rem;
+  margin-right: -1.6rem;
+
+  & > div:first-child {
+    ${breakpoints({
+      cssProp: 'width',
+      cssPropUnits: '%',
+      values: [{ [devices.default]: 100 }, { [devices.smallDevices]: 28 }],
+    })};
+  }
 `;
 
-export const FooterInfo = styled.div`
-  max-width: 27.2rem;
-  padding-right: 4.8rem;
+export const FooterItem = styled.div`
+  box-sizing: border-box;
+  padding-left: 1.6rem;
+  padding-right: 1.6rem;
+  ${breakpoints({
+    cssProp: 'width',
+    cssPropUnits: '%',
+    values: [{ [devices.default]: 100 }, { [devices.smallDevices]: 24 }],
+  })};
+`;
 
-  span {
-    color: ${(props) => props.theme.mainColor};
+export const CompanyWidget = styled.div`
+  h3 {
+    color: ${(props) => props.theme.titleColor};
+    font-size: 2.4rem;
+    font-weight: 600;
   }
 
   p {
-    color: ${(props) => props.theme.mainColor};
+    color: ${(props) => props.theme.fontColor};
     font-size: 1.6rem;
     font-weight: 400;
     line-height: 2.4rem;
-    margin-top: 2.4rem;
   }
 `;
 
-export const FooterLinks = styled.ul`
-  column-count: 2;
-  display: inline-block;
-  padding-left: 2.4rem;
+export const ListLink = styled.ul`
+  ${breakpoints({
+    cssProp: 'padding-left',
+    cssPropUnits: 'rem',
+    values: [{ [devices.default]: 0 }, { [devices.smallDevices]: 2.4 }],
+  })};
+
+  ${breakpoints({
+    cssProp: 'padding-top',
+    cssPropUnits: 'rem',
+    values: [{ [devices.default]: 1.2 }, { [devices.smallDevices]: 7.8 }],
+  })};
 
   li {
-    color: ${(props) => props.theme.mainColor};
-    font-size: 1.6rem;
-    font-weight: 600;
-    margin-bottom: 2.4rem;
-    margin-right: 0.8rem;
+    margin-bottom: 1.2rem;
+
+    a {
+      color: ${(props) => props.theme.fontColor};
+      font-size: 1.6rem;
+      font-weight: 400;
+      transition: color 0.2s ease-out;
+
+      &:hover {
+        color: ${(props) => props.theme.mainColor};
+      }
+    }
   }
+`;
+
+export const Language = styled.div`
+  ${breakpoints({
+    cssProp: 'padding-top',
+    cssPropUnits: 'rem',
+    values: [{ [devices.default]: 1.2 }, { [devices.smallDevices]: 7.8 }],
+  })};
+`;
+
+export const FooterBottom = styled.div`
+  background: url(${footerBg}) no-repeat scroll center 0;
+  height: 26.6rem;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+`;
+
+const drives = keyframes` 
+  0% {
+    left: -25%;
+  }
+  100% {
+    left: 100%;
+  }
+`;
+
+export const FooterBgOne = styled.div`
+  background: url(${carImg}) no-repeat center center;
+  width: 330px;
+  height: 105px;
+  background-size: 100%;
+  position: absolute;
+  bottom: 0;
+  left: 30%;
+  -webkit-animation: ${drives} 22s linear infinite;
+  animation: ${drives} 22s linear infinite;
+`;
+
+export const FooterBgTwo = styled.div`
+  background: url(${cycleImg}) no-repeat center center;
+  width: 88px;
+  height: 100px;
+  background-size: 100%;
+  bottom: 0;
+  left: 38%;
+  position: absolute;
+  -webkit-animation: ${drives} 30s linear infinite;
+  animation: ${drives} 30s linear infinite;
 `;
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,53 +3,74 @@ import { Link } from 'react-router-dom';
 import Dropdown from '../../common/Dropdown';
 import { Wrapper } from '../../styles/Utils';
 import { Logo } from '../Header/HeaderStyles';
-import { Container, Content, FooterLinks, FooterInfo } from './FooterStyles';
+import {
+  FooterBottom,
+  Container,
+  FooterBgOne,
+  FooterTop,
+  FooterBgTwo,
+  FooterItem,
+  CompanyWidget,
+  ListLink,
+  Language,
+} from './FooterStyles';
 
 const Footer = () => {
   return (
     <Container>
       <Wrapper>
-        <Content>
-          <FooterInfo>
-            <Logo to="/">
-              <span>Quizil</span>
-            </Logo>
-            <p>
-              Education and strategy for internet marketers to launch and scale
-              marking and business campaigns online.
-            </p>
-          </FooterInfo>
-          <FooterLinks>
-            <li>
-              <Link to="/">The Quizil Blog</Link>
-            </li>
-            <li>
-              <Link to="/">Teacher Resources</Link>
-            </li>
-            <li>
-              <Link to="/">Help Center</Link>
-            </li>
-            <li>
-              <Link to="/">Accessibility and Inclusion</Link>
-            </li>
-            <li>
-              <Link to="/">Terms of Service</Link>
-            </li>
-            <li>
-              <Link to="/">Privacy Policy</Link>
-            </li>
-            <li>
-              <Link to="/">Careers</Link>
-            </li>
-            <li>
-              <Link to="/">About Us</Link>
-            </li>
-          </FooterLinks>
-          <div>
-            <Dropdown values={['English', 'Vietnamese']} />
-          </div>
-        </Content>
+        <FooterTop>
+          <FooterItem>
+            <CompanyWidget>
+              <h3>Quizil</h3>
+              <p>
+                Education and strategy for internet marketers to launch and scale marking and business campaigns online.
+              </p>
+            </CompanyWidget>
+          </FooterItem>
+          <FooterItem>
+            <ListLink>
+              <li>
+                <Link to="/">Company</Link>
+              </li>
+              <li>
+                <Link to="/">Android App</Link>
+              </li>
+              <li>
+                <Link to="/">IOS App</Link>
+              </li>
+              <li>
+                <Link to="/">Desktop</Link>
+              </li>
+            </ListLink>
+          </FooterItem>
+          <FooterItem>
+            <ListLink>
+              <li>
+                <Link to="/">FAQ</Link>
+              </li>
+              <li>
+                <Link to="/">Term &amp; conditions</Link>
+              </li>
+              <li>
+                <Link to="/">Reporting</Link>
+              </li>
+              <li>
+                <Link to="/">Documentation</Link>
+              </li>
+            </ListLink>
+          </FooterItem>
+          <FooterItem>
+            <Language>
+              <Dropdown values={['English', 'Vietnamese']} />
+            </Language>
+          </FooterItem>
+        </FooterTop>
       </Wrapper>
+      <FooterBottom>
+        <FooterBgOne />
+        <FooterBgTwo />
+      </FooterBottom>
     </Container>
   );
 };
